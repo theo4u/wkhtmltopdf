29d8eb25f020e5c822c18a3f60a0fc64dd78c98b
glinford <gregory.linford@bbc.co.uk>
2016-11-18 11:01:04 +0000

    first etst

---
+++ wkhtmltopdf.js
@@ -0,0 +1,40 @@
+var os = require('os');
+var platform = os.platform();
+var wget = require('wget-improved');
+var arch = os.arch();
+var exec = require('child_process').exec;
+var src, output, cmd;
+
+if(platform === 'darwin'){ //OSX
+  output = 'wkhtmltopdf.pkg';
+  if(arch.indexOf('64') > -1){ //64bit
+    src = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_osx-cocoa-x86-64.pkg';
+  } else { //32bit
+    src = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_osx-carbon-i386.pkg';
+  }
+  cmd = "installer -pkg wkhtmltopdf.pkg -target /";
+} else if (platform === 'win32'){ //windows
+ // TO DO
+} else { //linux
+  output = 'wkhtmltopdf.tar.xz';
+  if(arch.indexOf('64') > -1){ //64bit
+    src = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz';
+  } else { //32bit
+    src = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-i386.tar.xz';
+  }
+  cmd = "tar -xvf wkhtmltopdf.tar.xz && cp ./wkhtmltopdf/bin/wkhtmlto* /usr/bin && cp ./wkhtmltopdf/bin/wkhtmlto* /usr/local/bin";
+}
+
+var download = wget.download(src, output, {});
+download.on('error', function(err) {
+    console.log(err);
+});
+download.on('end', function(output) {
+  exec(cmd, function(error, stdout, stderr){
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log('wkhtmltopdf Successfully installed');
+  });
+});
