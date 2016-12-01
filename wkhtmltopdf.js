102e382f52d97f6d1e7a78b11ed656df1498da24
Joshua Estrin Skrzypek <jskrzypek@gmail.com>
2016-12-01 15:10:22 +0200

    Upgrade wkhtmltopdf to 0.12.4

--- wkhtmltopdf.js
+++ wkhtmltopdf.js
@@ -8,9 +8,9 @@ var src, output, cmd;
 if(platform === 'darwin'){ //OSX
   output = 'wkhtmltopdf.pkg';
   if(arch.indexOf('64') > -1){ //64bit
-    src = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_osx-cocoa-x86-64.pkg';
+    src = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.4/wkhtmltox-0.12.4_osx-cocoa-x86-64.pkg';
   } else { //32bit
-    src = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_osx-carbon-i386.pkg';
+    src = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.4/wkhtmltox-0.12.4_osx-carbon-i386.pkg';
   }
   cmd = "installer -pkg wkhtmltopdf.pkg -target /";
 } else if (platform === 'win32'){ //windows
@@ -18,9 +18,9 @@ if(platform === 'darwin'){ //OSX
 } else { //linux
   output = 'wkhtmltopdf.tar.xz';
   if(arch.indexOf('64') > -1){ //64bit
-    src = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz';
+    src = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz';
   } else { //32bit
-    src = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-i386.tar.xz';
+    src = 'http://download.gna.org/wkhtmltopdf/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-i386.tar.xz';
   }
   cmd = "tar -xvf wkhtmltopdf.tar.xz && cp ./wkhtmltox/bin/wkhtmlto* /usr/bin && cp ./wkhtmltox/bin/wkhtmlto* /usr/local/bin";
 }
