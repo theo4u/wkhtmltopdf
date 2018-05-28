d8791faaa947abab8380fe21b12a8d45f8b8eadf
Theophilus Omorebee <theo4u@ymail.com>
2018-05-28 11:44:19 +0100

    fix: download just for linux 64 bit version

--- wkhtmltopdf.js
+++ wkhtmltopdf.js
@@ -1,45 +1,30 @@
-var os = require('os');
-var platform = os.platform();
-var wget = require('wget-improved');
-var arch = os.arch();
-var exec = require('child_process').exec;
-var src, output, cmd, suffix;
+var wget = require('wget-improved')
+var exec = require('child_process').exec
+var src, output, cmd, suffix
 
-var url = 'https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/';
-var version = '0.12.4';
+var url = 'https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/'
+var version = '0.12.4'
 
-if(platform === 'darwin'){ //OSX
-  output = 'wkhtmltopdf.pkg';
-  if(arch.indexOf('64') > -1){ //64bit
-    suffix = '_osx-cocoa-x86-64.pkg';
-  } else { //32bit
-    suffix = '_osx-carbon-i386.pkg';
-  }
-  cmd = "installer -pkg wkhtmltopdf.pkg -target /";
-} else if (platform === 'win32'){ //windows
- // TO DO
-} else { //linux
-  output = 'wkhtmltopdf.tar.xz';
-  if(arch.indexOf('64') > -1){ //64bit
-    suffix = '_linux-generic-amd64.tar.xz';
-  } else { //32bit
-    suffix = '_linux-generic-i386.tar.xz';
-  }
-  cmd = "tar -xvf wkhtmltopdf.tar.xz && cp ./wkhtmltox/bin/wkhtmlto* /usr/bin && cp ./wkhtmltox/bin/wkhtmlto* /usr/local/bin";
-}
+// linux
+output = 'wkhtmltopdf.tar.xz'
+// 64bit
+suffix = '_linux-generic-amd64.tar.xz'
+cmd = 'tar -xvf wkhtmltopdf.tar.xz'
 
-src = url + version + '/wkhtmltox-' + version + suffix;
+src = url + version + '/wkhtmltox-' + version + suffix
 
-var download = wget.download(src, output, {});
-download.on('error', function(err) {
-    console.log(err);
-});
-download.on('end', function(output) {
-  exec(cmd, function(error, stdout, stderr){
+console.log('installing linux binary 64bit of wkhtmltopdf');
+
+var download = wget.download(src, output, {})
+download.on('error', function (err) {
+  console.log(err)
+})
+download.on('end', function (output) {
+  exec(cmd, function (error, stdout, stderr) {
     if (error) {
-      console.error(`exec error: ${error}`);
-      return;
+      console.error(`exec error: ${error}`)
+      return
     }
-    console.log('wkhtmltopdf Successfully installed');
-  });
-});
+    console.log('wkhtmltopdf Successfully installed')
+  })
+})
